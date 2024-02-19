a072dda3bd11b28145e0eba2f5cbea78a6c15477
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-02-19 10:39:13 +0100

    generated getters and setters

--- src/BattleArena/Dwarf.java
+++ src/BattleArena/Dwarf.java
@@ -5,4 +5,17 @@ public class Dwarf {
 	private int damageOutput;
 	private boolean abilityPossible;
 	
+	public int getDamageOutput() {
+		return damageOutput;
+	}
+	public void setDamageOutput(int damageOutput) {
+		this.damageOutput = damageOutput;
+	}
+	public boolean isAbilityPossible() {
+		return abilityPossible;
+	}
+	public void setAbilityPossible(boolean abilityPossible) {
+		this.abilityPossible = abilityPossible;
+	}
+	
 }
