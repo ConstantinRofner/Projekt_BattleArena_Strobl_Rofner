ba6df8f70cf8ec4461057d31d024b2e34b6cc625
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-04 10:07:13 +0100

    added a constructor

--- src/BattleArena/Dwarf.java
+++ src/BattleArena/Dwarf.java
@@ -3,21 +3,12 @@ package BattleArena;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Dwarf extends Character{
-	
+
+
 	public Dwarf(String name) {
-		super(name);
-	}
-	
-	private boolean specialAbilityActive() {
-		if(this.getHealthPoints() > 50) {
-			this.setSpecialAbilityActive(true);
-		}
-		return true;
-	}
-	
-	private int attackValue = ThreadLocalRandom.current().nextInt(15, 25+1);
-	
-	public  int positiveAttackWithAbility(int attackValue) {
-		if(this.specialAbilityActive() == true &&  )
+		this.setName(name);
+		this.setAttackValue(ThreadLocalRandom.current().nextInt(15, 26));
+		this.setHealthPoints(100);
+		this.setSpecialAbilityActive(false);
 	}
 }
