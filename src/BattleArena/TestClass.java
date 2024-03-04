a9f714c22c9622c2632a3b6281fd9a21333522f2
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-04 10:35:42 +0100

    added a new Arena

--- src/BattleArena/TestClass.java
+++ src/BattleArena/TestClass.java
@@ -1,10 +1,14 @@
 package BattleArena;
 
+import java.awt.geom.Area;
+
 public class TestClass {
 
 	public static void main(String[] args) {
 		
 		Dragon d1 = new Dragon("Honig Zahn");
 		Dwarf z1 = new Dwarf("Erwin");
+		
+		Arena Arena = new Arena(d1, z1);
 	}
 }
