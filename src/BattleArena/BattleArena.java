b2031765ae6f5db10c9014da2a00d5b0f557a939
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-10 21:40:17 +0100

    corrected fighter to character

--- src/BattleArena/BattleArena.java
+++ src/BattleArena/BattleArena.java
@@ -5,13 +5,13 @@ public class BattleArena {
 	private Character c1;
 	private Character c2;
 	private Character Winner;
-	private boolean SelectedFighter;
+	private boolean SelectedCharacter;
 	
 	public BattleArena(Character c1, Character c2) {
 		super();
 		this.c1 = c1;
 		this.c2 = c2;
 		Winner = null;
-		SelectedFighter = randomFighter();
+		SelectedCharacter = randomCharacter();
 	}
 }
