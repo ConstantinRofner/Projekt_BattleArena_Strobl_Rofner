e1f8a253e69835a32727ca352dc4c77279874a5a
ConstantinRofner <Constantin.Rofner@hak-reutte.ac.at>
2024-03-11 10:02:55 +0100

    format changes

--- src/BattleArena/BattleFields.java
+++ src/BattleArena/BattleFields.java
@@ -2,11 +2,11 @@ package BattleArena;
 
 public class BattleFields {
 
-	    private Character character1;
-	    private Character character2;
+	private Character character1;
+	private Character character2;
 
-	    public BattleFields(Character charakter1, Character charakter2) {
-	        this.character1 = character1;
-	        this.character2 = character2;
-	    }
+    public BattleFields(Character charakter1, Character charakter2) {
+    	this.character1 = character1;
+    	this.character2 = character2;
+    }
 }
