8e5b54f153ea1db0c51c8b546b99da701070e4de
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-03-11 10:56:50 +0100

    added something in test class

--- src/BattleArena/BattleFields.java
+++ src/BattleArena/BattleFields.java
@@ -2,11 +2,10 @@ package BattleArena;
 
 public class BattleFields {
 
-	    private Character charakter1;
-	    private Character charakter2;
-
-	    public BattleFields(Character charakter1, Character charakter2) {
-	        this.charakter1 = charakter1;
-	        this.charakter2 = charakter2;
-	    }
+	public static void main(String[] args) {	
+		Character c = new Dwarf("Nevio");
+		System.out.println(c);
+		Character c2 = new Dragon("Zoe");
+		System.out.println(c2);
+	}
 }
