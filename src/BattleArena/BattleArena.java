3aa48d86c776569764365ecfaaa1db348d59c271
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-03-11 10:11:00 +0100

    created 2 new methods

--- src/BattleArena/BattleArena.java
+++ src/BattleArena/BattleArena.java
@@ -1,5 +1,7 @@
 package BattleArena;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class BattleArena {
 	
 	private Character c1;
@@ -7,6 +9,7 @@ public class BattleArena {
 	private Character Winner;
 	private boolean SelectedCharacter;
 	
+	// Konstruktor
 	public BattleArena(Character c1, Character c2) {
 		super();
 		this.c1 = c1;
@@ -14,10 +17,62 @@ public class BattleArena {
 		Winner = null;
 		SelectedCharacter = randomCharacter();
 	}
+	
+	/**
+	 * Method to find the character who starts 
+	 * @return
+	 */
+	private boolean randomCharacter() {
+		int random = ThreadLocalRandom.current().nextInt(1, 3); // Der Letzte Wert (3) ist exklusive!! d.h. entweder 1 oder 2.
+		if (random == 1) {
+			return true;
+		}else {
+		return false;
+	}
+}
+
+	//Getter + Setter
+	public Character getC1() {
+		return c1;
+	}
+	public void setC1(Character c1) {
+		this.c1 = c1;
+	}
+	public Character getC2() {
+		return c2;
+	}
+	public void setC2(Character c2) {
+		this.c2 = c2;
+	}
+	public Character getWinner() {
+		return Winner;
+	}
+	public void setWinner(Character winner) {
+		Winner = winner;
+	}
+	public boolean isSelectedCharacter() {
+		return SelectedCharacter;
+	}
+	public void setSelectedCharacter(boolean selectedCharacter) {
+		SelectedCharacter = selectedCharacter;
+	}
 
 
 
-	public static void main(String[] args) {
-		
+
+	
+	
+	/**
+	 * Method to find the winner of the fight
+	 * @return
+	 */
+	public Character endOfFightAndGetWinner() {
+		if(c1.getHealthPoints() == 0) {
+			return c1;
+		}else if(c2.getHealthPoints() == 0){
+			return c2;
+		} else {
+		return Winner;
+		}
 	}
 }
