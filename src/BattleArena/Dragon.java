f6b5229c522c0e7d50bf8f59a8a97f4f54fb14f0
Strobelix31 <luca.strobl@hak-reutte.ac.at>
2024-03-12 21:45:38 +0100

    changed some mistakes and completed toString

--- src/BattleArena/Dragon.java
+++ src/BattleArena/Dragon.java
@@ -7,7 +7,7 @@ public class Dragon extends Character {
 	    public Dragon(String name) {
 	        super(name);
 	    }
-	    
+
 	    @Override
 		public String getClassName() {
 			return "Dragon";
@@ -20,11 +20,9 @@ public class Dragon extends Character {
 	    @Override
 	    public void attack(Character enemy) {
 	        int angriffswert = ThreadLocalRandom.current().nextInt(20, 24+1);
-
 	        if (getSpecialAbilityActive()) {
 	            angriffswert -= (int) (Math.random() * 11);
 	        }
-
 	        enemy.getDamage(angriffswert);
 	    }
 
@@ -41,27 +39,37 @@ public class Dragon extends Character {
 			}
 		}
 
-	    	/**
-	     	* method to activate the SpecialAbility and give 10 bonus health to the dragon
-	     	*/
+	    /**
+	     * method to activate the SpecialAbility and give 10 bonus health to the dragon
+	     */
 		public boolean activateSpecialAbility() {
 				this.setSpecialAbilityActive(true);
 				this.setBonusHealth(getBonusHealth()+10);
 				return true;
-}
+		}
 
-			/**
-			 * method to deactivate SpecialAbilitry and remove 10 bonus health from the dragon
-			 */
+		/**
+		 * method to deactivate SpecialAbilitry and remove 10 bonus health from the dragon
+		 */
 		public boolean deactivateSpecialAbility() {
 			this.setSpecialAbilityActive(false);
 			if(!((this.getBonusHealth() - 10) >= 0)) {
-			this.setBonusHealth(getBonusHealth()-10);
-		}else {
+				this.setBonusHealth(getBonusHealth()-10);
+			}else {
 			this.setBonusHealth(0);
 			}
-			return true;
-		}
+		return true;
+	}
+		
+		/**
+		 * toString -- for the console of the game
+		 */
+		@Override
+		public String toString() {
+			return "Name des Drachens: " + this.getName() + "\n " + "Schaden der Attacke: " + this.getAttackValue() + "\n " 
+					+ "Anzahl der Leben: "+ this.getHealthPoints() + "\n " 
+					+ "Aktivitätsstatus der Spezialfähigkeit: " + getSpecialAbilityActive() + "\n " 
+					+ "Name der Klasse: " + getClass() + "\n " 
+					+ "Hashcode: " + hashCode() + "\n ";
+			}
 }
-
- 
